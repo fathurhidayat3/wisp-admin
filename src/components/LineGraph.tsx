c116bb136a65ff5021970b3d40643acdcfd0ac26
fathur.hidayat <fathur.hidayat@kumparan.com>
2019-11-17 12:36:59 +0700

    update LineGraph

--- src/components/LineGraph.tsx
+++ src/components/LineGraph.tsx
@@ -1,8 +1,6 @@
 import * as React from "react";
-import styled from "styled-components";
 import { Line } from "react-chartjs-2";
 
-import Text from "./Text";
 import {
   graphInitialData,
   graphOptions,
@@ -11,7 +9,6 @@ import {
 
 type LineGraphProps = {
   streamData: string | number;
-  title: string;
   height?: number;
   width?: number;
   options: any;
@@ -27,26 +24,14 @@ const LineGraph = (props: LineGraphProps) => {
   }, [props.streamData]);
 
   return (
-    <GraphContainer>
-      <Text as="h4">{props.title}</Text>
-      <Line
-        height={props.height}
-        width={props.width}
-        // @ts-ignore
-        data={graphData}
-        options={{ ...graphOptions, ...props.options }}
-      />
-    </GraphContainer>
+    <Line
+      height={props.height}
+      width={props.width}
+      // @ts-ignore
+      data={graphData}
+      options={{ ...graphOptions, ...props.options }}
+    />
   );
 };
-
-const GraphContainer = styled.div`
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-
-  padding: 0 8px;
-`;
 
 export default LineGraph;
